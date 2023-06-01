3e2146cfe4961a176b371e8fae7d6fedcc481402
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-01 16:09:43 +0100

    added update character method

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -35,5 +35,16 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(await characterService.AddCharacter(newCharacter));
         }
+
+        [HttpPut]
+        public async Task <ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter (UpdateCharacterDto updatedCharacter)
+        {
+            var response = await characterService.UpdateCharacter(updatedCharacter);
+            if (response.Data is null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
     }
 }
