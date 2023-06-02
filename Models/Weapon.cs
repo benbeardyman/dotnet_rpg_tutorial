a160ff0bbcec948f318bc9f10c56a732bbf6aba5
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-02 16:27:47 +0100

    added weapon model

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -7,6 +7,10 @@ namespace dotnet_rpg.Models
 {
     public class Weapon
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Damage { get; set; }
+        public Character? Character { get; set; }
+        public int CharacterId { get; set; }
     }
 }
